80545c6db9f671a8934ae560e8706bf0e31b76e3
user_2 <user_2@FACE-68>
2019-08-20 16:26:40 +0530

    jjjj

--- src/main/java/main/SampleProject.java
+++ src/main/java/main/SampleProject.java
@@ -4,7 +4,7 @@ public class SampleProject {
 	//print  Hello world! in the console
 	public static String printHello() {
 		//print Hello world! in the console!
-		String c = "Hello rvabddld!";
+		String c = "Hello World!";
 		return c;
 	}
 	public static int add(int a,int b) {
@@ -13,6 +13,6 @@ public class SampleProject {
 	}
 	public static void main(String args[]) {
 		System.out.println(printHello());
-		System.out.println(add(23,45));
+		System.out.println(add(29,45));
 	}
 }
